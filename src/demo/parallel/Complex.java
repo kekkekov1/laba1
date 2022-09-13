ebd0189b9dc8ba8883f7bbdefd1e9197a34c4167
Artyom <artem05071977@gmail.com>
2022-09-13 20:10:26 +0300

    new operation

--- src/demo/parallel/Complex.java
+++ src/demo/parallel/Complex.java
@@ -85,6 +85,11 @@ public class Complex {
         return this;
     }
 
+    public Complex sub(Complex b) {
+        re -= b.re;
+        im -= b.im;
+        return this;
+    }
     /**
      * Square of Complex object's length, we're using square of length to 
      * eliminate the computation of square root
